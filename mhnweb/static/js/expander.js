40b3d9378f38d55b18addaad068e4ac7c5f203ec
eRestin <lukasz.iljaszewicz@gmail.com>
2014-03-20 15:38:31 +0100

    Static added

---
+++ mhnweb/static/js/expander.js
@@ -0,0 +1,16 @@
+function toggleSlides(){
+	$('.toggler').click(function(e){
+		var id=$(this).attr('id');
+		var widgetId=id.substring(id.indexOf('-')+1,id.length);
+		$('#'+widgetId).slideToggle();
+		$(this).toggleClass('sliderExpanded');
+		$('.closeSlider').click(function(){
+				$(this).parent().hide('slow');
+				var relatedToggler='toggler-'+$(this).parent().attr('id');
+				$('#'+relatedToggler).removeClass('sliderExpanded');
+		});
+	});
+};
+$(function(){
+	toggleSlides();
+});
